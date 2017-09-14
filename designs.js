ddacb6d6d4caa1640c487e45d8d8173848cd9a43
Aaron Neff <arneff@gmail.com>
2017-09-13 19:37:00 -0600

    Added drawing functionality using color picker and added comments for usability

--- designs.js
+++ designs.js
@@ -11,14 +11,23 @@ function makeGrid() {
   event.preventDefault(); //prevent auto browser refresh
   $('#pixel_canvas tr').remove();//clear table on resubmission
   let height = $("#input_height").val();//declare row variable
-  let width = $("#input_width").val();//delcare column variable
+  let width = $("#input_width").val();//declare column variable
+  let color = $('#colorPicker').val();//declare color variable
 
   for(let i = 0; i < height; i++){
     $('#pixel_canvas').find('tbody').append('<tr></tr>');
-  }//end row for loop
+  }//end row loop
 
   for(let i = 0; i < width; i++){
     $('#pixel_canvas').find('tr').append('<td></td>');
-  }//column loop
+  }//end column loop
+
+  $( "#colorPicker" ).change(function() {
+    color = $(this).val();
+  });//listens for change in color value
+
+  $('#pixel_canvas').find('td').click(function(){
+    $(this).css('background-color', color);
+  });//sets background-color of selected cell onclick
 
 } //end makeGrid
